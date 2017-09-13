6d4cf0b30e9ea773f7d2ffe93de3b608df2f345a
paul5254 <wangpc5254@163.com>
2017-09-13 23:17:21 +0800

    test
    Signed-off-by: paul5254 <wangpc5254@163.com>

--- src/com/example/web/BeerSelect.java
+++ src/com/example/web/BeerSelect.java
@@ -11,15 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 import sun.print.resources.serviceui;
 
 public class BeerSelect extends HttpServlet {
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		resp.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
 		out.println("Beer Selection Advice<br>");
 		String c = req.getParameter("color");
-		out.println("<br>Got beer color " + c);
+		out.println("<br>Got beer color " + c + "Õı¥Û≈Ù");
 	}
 
 }
